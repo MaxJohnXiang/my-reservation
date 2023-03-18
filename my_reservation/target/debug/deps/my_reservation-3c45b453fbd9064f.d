18194190c9963926c7c98ddee56a4228ff6c4e12
MaxJohnXiang <seemaxagain@outlook.com>
2023-03-18 14:14:46 +0800

    init

---
+++ my_reservation/target/debug/deps/my_reservation-3c45b453fbd9064f.d
@@ -0,0 +1,5 @@
+/Users/birley/code/rust/myReservation/my_reservation/target/debug/deps/my_reservation-3c45b453fbd9064f.rmeta: src/lib.rs
+
+/Users/birley/code/rust/myReservation/my_reservation/target/debug/deps/my_reservation-3c45b453fbd9064f.d: src/lib.rs
+
+src/lib.rs:
---
+++ my_reservation/target/debug/deps/my_reservation-544dec79cdba2c2c.d
@@ -0,0 +1,5 @@
+/Users/birley/code/rust/myReservation/my_reservation/target/debug/deps/my_reservation-544dec79cdba2c2c.rmeta: src/lib.rs
+
+/Users/birley/code/rust/myReservation/my_reservation/target/debug/deps/my_reservation-544dec79cdba2c2c.d: src/lib.rs
+
+src/lib.rs:
